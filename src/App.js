e7beaa50caf2ca84cd2bee6b9857056e498a1982
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-02-25 16:06:09 +0800

    checkout init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './component/Header';
+import Home from './component/Home';
+import Checkout from './component/Checkout';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/checkout" element={(
+            <>
+              <Checkout />
+            </>
+          )} />
+          <Route path="/" element={(
+            <>
+              <Home />
+            </>
+          )} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/component/Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import '../css/Checkout.css'
+
+function Checkout() {
+    return (
+        <div className="checkout">
+
+            <h1>I AM CHECKOUT PAGE</h1>
+        </div>
+    )
+}
+
+export default Checkout
