edc13e3bc2736dbdda824c958f092e07256451ab
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-02-25 18:12:41 +0800

    indentation

--- src/component/Subtotal.js
+++ src/component/Subtotal.js
@@ -7,17 +7,19 @@ function Subtotal() {
     return (
         <div className="subtotal">
             <CurrencyFormat
-                renderText={(value) => (
-                    <>
-                        <p>
-                            Subtotal (0 items);
-                            <strong>0</strong>
-                        </p>
-                        <small className="subtotal__gift">
-                            <input type="checkbox" /> This order contains a gift
-                        </small>
-                    </>
-                )}
+                renderText={
+                    (value) => (
+                        <>
+                            <p>
+                                Subtotal (0 items);
+                                <strong>0</strong>
+                            </p>
+                            <small className="subtotal__gift">
+                                <input type="checkbox" /> This order contains a gift
+                            </small>
+                        </>
+                    )
+                }
                 decimalScale={2}
                 value={0}
                 displayType={"text"}
